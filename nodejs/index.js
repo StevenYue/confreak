dce176ab97679eb130a4cd89fe15d44a9b434314
Steeng <steven.yuexi@gmail.com>
2016-08-29 00:03:14 -0400

    login almost finished

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,67 +7,157 @@ var Session = require("client-sessions");
 var g_confreakdb = require("mongoose");
 var g_app = Express();
 
+
 //connect mongodb
 g_confreakdb.connect("localhost:10000/confreakdb");
-var UserModel = g_confreakdb.model("users", {name: String, email: String, password: String});
+var UserModel = g_confreakdb.model("freak", {name: String, email: String, password: String});
 
 //g_app set up
 g_app.set("view engine", "ejs");
 g_app.use(BodyParser.json());
 g_app.use(BodyParser.urlencoded({extended: true}));
+g_app.use(Session({
+  cookieName: 'session',
+  secret: 'dhgaugahnguroqarnaf',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
 g_app.listen(8000, function (){
     console.log("confreak start listening port 8000");        
 });
+
 //end of g_app set up
+
+//Constants
+const LOGIN_ERROR_EMAIL_NOT_FOUND = -1;
+const LOGIN_ERROR_WRONG_PASSWORD = -2;
+
+const CHECK_AVAILABILITY_OK = 0;
+const CHECK_AVAILABILITY_BAD = -1;
+
+//end of Constants
 
 
 //requests handlers
 g_app.get("/", function(req, res){
-    res.render("index");
+    res.render("index.ejs");
 });
 
-g_app.get("/register", function(req, res){
-    res.render("register");
+g_app.get("/register.ejs", function(req, res){
+    res.render("register.ejs");
 });
 
 g_app.get("/application.ejs", function(req, res){
-    res.render("application");
+    res.render("application.ejs");
 });
 
 g_app.post("/login", function(req, res){
+    console.log("Login Request:", req.body);
     var user = createUser(req.body.email, '', '');
     findUserModel(user, function(re){
-
         if ( re.errno )
         {
             console.log("login error");
         }
         else
         {
-            if ( req.body.password === re.obj.password )
+            if ( re.obj )
             {
-                res.render("application", {user: re.obj});
+                if ( req.body.password === re.obj.password )
+                {
+                    req.session.email = req.body.email;
+                    req.session.password = req.body.password;
+                    res.redirect("/application.ejs?name=" + re.obj.name);
+                }
+                else
+                {
+                    res.redirect("/?errno=" + LOGIN_ERROR_WRONG_PASSWORD);
+                }
             }
             else
             {
-                res.render("index", {errno: -1});
+                res.redirect("/?errno=" + LOGIN_ERROR_EMAIL_NOT_FOUND);
             }
         }
     });
 });
 
 g_app.post("/create", function(req, res){
+    console.log("Create Freak Request:", req.body);
     var user = createUser(req.body.email, req.body.password, req.body.userName);
-    var re = findUserModel(user, returnCallBack);
-    if ( !re )
-    {
-        insertUserModel(user);
-        res.render("application", {user: user});
-    }
-    else
-    {
-       console.log("Creatation error"); 
-    }
+    findUserModel(user, function(re){
+        if ( re.errno )
+        {
+            console.log("Creatation error"); 
+        }
+        else
+        {
+            if ( !re.obj )
+            {
+                insertUserModel(user);
+                req.session.email = req.body.email;
+                req.session.password = req.body.password;
+                res.redirect("/application.ejs?name=" + req.body.userName);
+            }
+            else
+            {
+                console.log("User %j existed aleady, creatation failed", user);
+            }
+        }
+    });
+});
+
+g_app.post("/checkAvailablity", function(req, res){
+    console.log("checkAvailablity Request:", req.body);
+    var user = createUser(req.body.email, '', '');
+    findUserModel(user, function(re){
+        if ( re.errno )
+        {
+            console.log("checkAvailablity error");
+        }
+        else
+        {
+            var checkResult = {errno: CHECK_AVAILABILITY_OK};
+            if ( re.obj )
+            {
+                checkResult.errno = CHECK_AVAILABILITY_BAD;   
+            }
+            res.send(checkResult);
+        }
+    });   
+});
+
+g_app.post("/dummy", function(req, res){
+    console.log("dummy Request:", req.body)
+    var user = createUser(req.session.email, '', '');
+    findUserModel(user, function(re){
+        if ( re.errno )
+        {
+            console.log("sessionVerify error");
+        }
+        else
+        {
+            if ( re.obj )
+            {
+                if ( re.obj.password === req.session.password )
+                {
+                    console.log("sessionVerify passed");
+                }
+                else
+                {
+                    console.log("sessionVerify failed, wrong password");
+                    res.redirect("/index");
+                }   
+            }
+            else
+            {
+                console.log("sessionVerify failed, not a valid user");
+                res.send({a:1});
+                console.log("fuck");
+            }
+            
+        }
+    });   
 
 });
 
@@ -106,15 +196,10 @@ function findUserModel(user, fn)
         }
         else
         {
-            console.log("User obj found: %j", userObj);
+            console.log("Freak obj found: %j", userObj);
         }
         fn({errno: err, obj: userObj});
     });
-}
-
-function returnCallBack(obj)
-{
-    return obj;
 }
 
 //end of subs
--- nodejs/views/register.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  alert("Test, Do not forget to delete");
-});
-
-//Steeng create at: Fri Aug 19 18:29:49 EDT 2016
