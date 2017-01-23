6a24528af90c83c99c86225de0771fc402ae58ec
Steeng <steven.yuexi@gmail.com>
2017-01-22 20:44:17 -0500

    config file loader done

--- cpp/confreak.m.cpp
+++ cpp/confreak.m.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <config_loader.h>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "hello confreak" << std::endl; 
+    if ( argc != 2 )
+    {
+        std::cout << "Confreak Usage: confreak.tsk confreak.cfg" << std::endl;
+        exit(1);
+    }
+    try
+    {
+        Config* config = Config::getInstance(argv[1]);
+        std::cout << "email:    " << (*config)["LOGIN"]["email"].asString() << std::endl;
+        std::cout << "password: " << (*config)["LOGIN"]["password"].asString() << std::endl;
+        std::cout << "password: " << (*config)["LOGIN"]["password"].asInt() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception Caught: " << e.what() << std::endl;
+        exit(1);
+    }
     return 0;
 }
